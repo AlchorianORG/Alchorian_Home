fc4e93c9dcfda0e408ac986cb5e1bc400d21cdf5
Pranay Prasad <pranayprasad42+work@gmail.com>
2025-12-30 01:35:27 +0000

    Background Change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Aria from "@/components/Aria";
 import { SpotlightNewDemo } from "@/components/Hero";
 import MeetIntelligence from "@/components/MeetIntelligence";
 import { NavbarDemo } from "@/components/Navbar";
@@ -5,11 +6,12 @@ import WhyAlchorian from "@/components/WhyAlchorian";
 
 export default function Home() {
   return (
-    <>
+    <div className="bg-gradient-to-br from-purple-950/50 via-slate-950 to-slate-950">
     <NavbarDemo/>
     <SpotlightNewDemo/>
     <MeetIntelligence/>
     <WhyAlchorian/>
-    </>
+    {/* <Aria/> */}
+    </div>
   );
 }
---
+++ src/components/Aria.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Aria = () => {
+    return (
+        <div className='h-[50vh] pt-10 bg-gradient-to-br from-purple-950/50 via-slate-950 to-slate-950'>
+            <div className='max-w-7xl mx-auto'>
+
+            <div className="section_title  ">
+                <h1 className="text-2xl md:text-5xl font-bold text-center 
+               leading-[1.2] pb-1
+               bg-clip-text text-transparent 
+               bg-linear-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+                    Hiring Was Never Built for Candidates.<br/> ARIA Is.
+                </h1>
+
+            </div>
+            <div className="aria_info mt-10 mx-auto max-w-5xl  ">
+                <p className="mt-2 text-center  text-2xl text-gray-300">Most platforms treat candidates as data points to be filtered. ARIA changes that. It helps candidates understand real recruiter expectations, receive actionable feedback, and prepare with direction instead of guesswork.</p>
+            </div>
+               </div>
+        </div>
+    )
+}
+
+export default Aria
