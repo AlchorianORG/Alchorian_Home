0d26631afe6c6cf73f48872ae6861b21f5c4361e
Pranay Prasad <pranayprasad42+work@gmail.com>
2026-03-12 09:33:47 +0000

    Production Aria link

--- src/constants/Links.tsx
+++ src/constants/Links.tsx
@@ -1,3 +1,2 @@
-// export const ARIA_LINK = 'https://ariademo.netlify.app';
-export const ARIA_LINK = "#aria"
+export const ARIA_LINK = 'https://aria.alchorian.com';
 export const TYPRFORM_LINK = 'https://form.typeform.com/to/Cbp7jrYI'
