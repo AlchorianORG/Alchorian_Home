c75e5ae2682fa49e3bc0e3adfc6f58b97354427f
Pranay Prasad <pranayprasad42+work@gmail.com>
2026-03-13 11:25:18 +0000

    Survey Link

--- src/constants/Links.tsx
+++ src/constants/Links.tsx
@@ -1,2 +1,2 @@
 export const ARIA_LINK = 'https://aria.alchorian.com';
-export const TYPRFORM_LINK = 'https://form.typeform.com/to/Cbp7jrYI'
+export const TYPRFORM_LINK = 'https://linktr.ee/alchorian'
